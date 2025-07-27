250fffad9ae1cfc8f660a1295b40b7e1b664ea2f
Henrique Caetano <59708919+Rick-Caetano@users.noreply.github.com>
2025-07-27 13:00:35 -0300

    Delete api/livecd.py

--- api/livecd.py
+++
@@ -1,8 +0,0 @@
-def handler(request, response):
-    response.setHeader("Content-Type", "text/plain")
-    response.send("""#!/bin/bash
-sudo apt update
-sudo apt install -y htop curl neofetch
-sudo apt install aircrack-ng
-echo "Configuração concluída!"
-""")
