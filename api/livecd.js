f2b8fb6ca02fae68cbdbcbf24f568248c71cf2d7
Henrique Caetano <59708919+Rick-Caetano@users.noreply.github.com>
2025-07-27 12:58:15 -0300

    Update livecd.js

--- api/livecd.js
+++ api/livecd.js
@@ -2,6 +2,7 @@ export default function handler(req, res) {
   const script = `#!/bin/bash
 sudo apt update
 sudo apt install -y htop curl neofetch
+sudo apt install aircrack-ng
 echo "Configuração concluída!"`;
 
   res.setHeader("Content-Type", "text/plain");
